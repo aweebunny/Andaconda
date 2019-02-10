5ed88e6e78e25af5a2f29e99a9cacdf39fe4d3fa
pbmsa <psalas2@uco.edu>
2019-02-10 09:23:35 -0600

    Changes made since 2am

--- luckyusagi/Scripts/Activate.ps1
+++ luckyusagi/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Users\pbmsa\hacklahoma\luckyusagi"
+$env:VIRTUAL_ENV="C:\Users\pbmsa\hacklahoma\djangogirls\luckyusagi"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
